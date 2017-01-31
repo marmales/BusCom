168e81d23cc5f0bae64e2db8032fb9a5825c0f9b
marmales <mar.maleszewski@gmail.com>
2017-01-31 18:22:49 +0100

    example layout (in Project/List)

---
+++ Source/BusCom/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusCom.Controllers
+{
+    public class ProjectController : Controller
+    {
+        IProjectRepository repository;
+        public ProjectController(IProjectRepository projectsParam)
+        {
+            repository = projectsParam;
+        }
+        public ActionResult List()
+        {
+            return View(repository.projects);
+        }
+    }
+}
